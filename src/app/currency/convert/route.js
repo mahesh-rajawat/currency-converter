9da479165f126afd9f184edadea31d5336d4ff14
mahesh.singh <maheshrajawat@gmail.com>
2023-03-27 17:57:29 +0530

    try catch aded

--- src/app/currency/convert/route.js
+++ src/app/currency/convert/route.js
@@ -12,65 +12,66 @@ export async function POST(req) {
     	const dataObject = JSON.parse(jsonData);
 		const url = process.env.CURRENCY_URL + `?to=${dataObject.to.toUpperCase()}&from=${dataObject.from.toUpperCase()}&amount=${dataObject.amount}`;
 		const cachedData = readFromCache(url);
-		
-		const res = cachedData === null ? await fetch(url, {
-			next: { revalidate: 60 },
-			method: 'GET',
-			headers: {
-			'Content-Type': 'application/json',
-			'apikey': process.env.DATA_API_KEY,
-			},
-		}) : cachedData;
-
-		// Server response from cache if same request exists.
-		if (cachedData !== null) {
-			const resultWithFormatPrice = cachedData.status == 200 ?
-				formatCurrency(cachedData, dataObject.to) : 
-				cachedData;
-			return NextResponse.json(
-				resultWithFormatPrice,
-				{
-				  status: cachedData.status,
-				  headers: {
-					'Server-Timing': `api-cache;dur=${performance.now() - startExcecution};desc=HIT`
-				  },
+		try {
+			const res = cachedData === null ? await fetch(url, {
+				method: 'GET',
+				headers: {
+				'Content-Type': 'application/json',
+				'apikey': process.env.DATA_API_KEY,
 				},
-			);
-		}
-		// Serve response from server anc cache the response
-		if (res.status == 200) {
-			const responseData = await res.json();
-			if (cachedData === null) {
-				responseData['status'] = res.status;
-				writeToCache(url, responseData);
+			}) : cachedData;
+			// Server response from cache if same request exists.
+			if (cachedData !== null) {
+				const resultWithFormatPrice = cachedData.status == 200 ?
+					formatCurrency(cachedData, dataObject.to) : 
+					cachedData;
+				return NextResponse.json(
+					resultWithFormatPrice,
+					{
+					status: cachedData.status,
+					headers: {
+						'Server-Timing': `api-cache;dur=${performance.now() - startExcecution};desc=HIT`
+					},
+					},
+				);
 			}
-			const resultWithFormatPrice = formatCurrency(responseData, dataObject.to);
-			
-			return NextResponse.json(
-				resultWithFormatPrice,
-				{
-				  status: 200,
-				  headers: {
-					'Server-Timing': `api-server;dur=${performance.now() - startExcecution};desc=CACHE-MISS`,
-				  },
-				},
-			);
-		} else {
-			const responseData = await res.json();
-			responseData['status'] = res.status;
-			if (cachedData === null) {
+			// Serve response from server anc cache the response
+			if (res.status == 200) {
+				const responseData = await res.json();
+				if (cachedData === null) {
+					responseData['status'] = res.status;
+					writeToCache(url, responseData);
+				}
+				const resultWithFormatPrice = formatCurrency(responseData, dataObject.to);
+				
+				return NextResponse.json(
+					resultWithFormatPrice,
+					{
+					status: 200,
+					headers: {
+						'Server-Timing': `api-server;dur=${performance.now() - startExcecution};desc=CACHE-MISS`,
+					},
+					},
+				);
+			} else {
+				const responseData = await res.json();
 				responseData['status'] = res.status;
-				writeToCache(url, responseData);
+				if (cachedData === null) {
+					responseData['status'] = res.status;
+					writeToCache(url, responseData);
+				}
+				return NextResponse.json(
+					responseData,
+					{
+					status: res.status,
+					headers: {
+						'Server-Timing': `api-server;dur=${performance.now() - startExcecution};desc=CACHE-MISS`,
+					},
+					},
+				);
 			}
-			return NextResponse.json(
-				responseData,
-				{
-				  status: res.status,
-				  headers: {
-					'Server-Timing': `api-server;dur=${performance.now() - startExcecution};desc=CACHE-MISS`,
-				  },
-				},
-			);
+		} catch (error) {
+			return NextResponse.json({ errors: 'Api unable to fetch'});
 		}
       	
     } else {
